f090432dbeaa4dc9c7c140a770c34de0a2027b62
FakinGOD <desarrolladorcadiz@gmail.com>
2016-02-16 16:16:40 +0100

    Actualización 1

---
+++ app/src/main/java/com/salesianos/jose/polizas/Polizas.java
@@ -0,0 +1,13 @@
+package com.salesianos.jose.polizas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Polizas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_polizas);
+    }
+}
